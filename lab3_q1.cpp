b80095a543021723546990c4c26320d210e44b12
HarshaIndukuri <42237438+HarshaIndukuri@users.noreply.github.com>
2018-08-17 11:20:20 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,34 @@
+//This is the library
+#include<iostream>
+using namespace std;
+
+int main(){
+cout <<"Yo"<<endl;
+//Declaring variables of all types
+
+int a = 8;
+double b = 3.78;
+float d = 3.14156;
+bool t = 0;
+char pi = 'c';
+//Displaying all the variables
+cout << a<<endl;
+cout << pi<<endl;
+cout << d<<endl;
+cout << b<<endl;
+cout << t<<endl;
+// Showing the size of all the variables
+
+cout <<"Size of a = "<< sizeof(a)<<endl;
+
+cout <<"Size of pi = "<< sizeof(pi)<<endl;
+
+cout <<"Size of d = "<< sizeof(d)<<endl;
+
+cout <<"Size of b = "<< sizeof(b)<<endl;
+
+cout <<"Size of t ="<<sizeof(t)<<endl;
+
+return 0;
+}
+
---
+++ lab3_q2.cpp
@@ -0,0 +1,47 @@
+//This is the library
+#include<iostream>
+using namespace std;
+
+int main(){
+cout <<"Arithmetic Operators in C++"<<endl;
+
+//Operations for integers
+
+int a = 156;
+int b = 13;
+int sum = a+b;
+int product = a*b;
+int quotient = a/b;
+int difference = a-b;
+int remainder = a%b;
+
+cout<<"a is 156"<<endl;
+cout<<"b is 13"<<endl;
+
+cout<<"Their sum is "<<sum<<endl;
+cout<<"Their product is "<<product<<endl;
+cout<<"Their difference is "<<difference<<endl;
+cout<<"a divided by b is "<<quotient<<endl;
+cout<<"The remainder is "<<remainder<<endl;
+
+//Operations for decimal numbers
+float c = 3.14;
+float d = 1.618;
+float Sum = c+d;
+float Product = c*d;
+float Quotient = c/d;
+float Difference = c-d;
+
+cout<<"c is 3.14"<<endl;
+cout<<"d is 1.618"<<endl;
+
+cout<<"Their sum is "<<Sum<<endl;
+cout<<"Their product is "<<Product<<endl;
+cout<<"Their difference is "<<Difference<<endl;
+cout<<"a divided by b is "<<Quotient<<endl;
+
+
+
+
+}
+
